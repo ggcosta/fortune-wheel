e10712256a6c7fe8d8cd2259f1e52cfdfc894dd9
Gonçalo Costa <goncaloc@live.com>
2024-07-15 13:16:00 +0100

    error testing

--- app/api/prizes/route.ts
+++ app/api/prizes/route.ts
@@ -29,14 +29,15 @@ export async function POST(request: Request) {
     const { prize } = await request.json();
     console.log(prize);
     const prizeIndex = prizes.prizes_given.findIndex((item: any) => item.name === prize);
-
+    console.log("test1")
     if (prizeIndex !== -1) {
       prizes.prizes_given[prizeIndex].amountGiven += 1;
     } else {
       prizes.prizes_given.push({ name: prize, amountGiven: 1 });
     }
-
+    console.log("test2")
     writePrizes(prizes);
+    console.log("test3")
     return NextResponse.json(prizes);
   } catch (error) {
     return NextResponse.json({ error: 'Invalid request data' }, { status: 400 });
